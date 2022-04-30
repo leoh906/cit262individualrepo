6615ef87316cfb00b04b570f1441091626e94ee0
Leo Harrison <leo.harrison96@gmail.com>
2022-04-30 08:08:12 -0600

    login-request update 4-28-22

--- test/hello.test.js
+++ test/hello.test.js
@@ -1,1 +1,11 @@
-it ('Should say hello', () =>{console.log('Hello Leo')});
+// it ('Should say hello', () =>{console.log('Hello Leo')});
+import login from '../utils/login.js';
+
+it ('Should get login token',async()=>{
+    const loginRequest = {
+        userName:"har21076@byui.edu",
+        password: "NPR@lei56mynom14",
+    }
+    
+    await login(loginRequest);
+})
---
+++ utils/login.mjs
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch';
+// mjs stands for module js, we can't really work without it
+const login = async (userName, password) => {
+
+// We are making a promise, will do something, just not right away
+// Valid JSON quotes are always double spaced
+
+const options = {
+    method: 'POST', // A post request gets data
+    body: JSON.stringify({userName, password}), // The parameters are in shorthand, userName:userName, password:password
+    headers: {
+        'Content-Type':'application/json',
+        'Accept':'Text'
+    }
+};
+
+    const apiResponse = await fetch('https://dev.stedi.me/login', options); // If we don't do an await, we will get a promise instead
+    const loginResponse = await apiResponse.text();
+
+    return loginResponse;
+
+}
+
+export {login};
+
+
